5419fca5331aa8724b86ddd2c74bc171472b04dc
Harsh Jain <hvj.techture@gmail.com>
2022-04-10 16:52:38 +0530

    Navbar and router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Login from "./Login";
+import Register from "./Register";
+import NoMatchPage from "./NoMatchPage";
+import Dashboard from "./Dashboard";
+import { HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <Login />
+    <HashRouter>
+      <NavBar />
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/" exact={true} component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="*" component={NoMatchPage} />
+        </Switch>
+      </div>
+    </HashRouter>
   );
 }
 
